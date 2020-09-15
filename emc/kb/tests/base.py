48307bc884b0c14bca27b94686ffa44398727b9f
adam139 <yuejun.tang@gmail.com>
2020-09-15 21:36:41 +0800

    add test base

---
+++ emc/kb/tests/base.py
@@ -0,0 +1,7 @@
+#-*- coding: UTF-8 -*-
+from emc.kb import log_session as Session
+
+
+TABLES = ['Fashej','Jieshouj','Fashetx','Jieshoutx','Lvboq','Dianxingtxzyzk',
+                'Tianxianzk','Jieshoujzk','Fashejzk','Ceshishysh',
+                'Ceshiry','Ceshiff','Ceshibg','Ceshixm']
