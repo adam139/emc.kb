7a03a7eb315c696f5537a929e2e505d5c4a2d3c0
adam139 <yuejun.tang@gmail.com>
2020-09-18 11:19:55 +0800

    update db tools script

--- emc/kb/initdb.py
+++ emc/kb/initdb.py
@@ -3,18 +3,27 @@ from emc.kb import log_session as Session
 from emc.kb import ora_engine as engine
 from  emc.kb import ORMBase as Base
 from  emc.kb.tests.base import TABLES
+from  emc.kb.tests.base import LOGTABLES
 
+actions = ["create","drop","empty"]
 
 class DbTools(object):
+
 
     def drop_tables(self,tbls=None):
         """drop all db tables
         """
-
+        if not bool(tbls):
+            tbls = TABLES       
         for tb in tbls:
-            import_str = "from %(p)s import %(t)s" % dict(p='emc.kb.mapping_db',t=tb) 
+            import_str = "from %(p)s import %(t)s as tablecls" % dict(p='emc.kb.mapping_db',t=tb) 
             exec import_str
-        Base.metadata.drop_all(engine)                                
+            tablecls.__table__.drop(engine)
+        for tb in LOGTABLES:
+            import_str = "from %(p)s import %(t)s as tablecls" % dict(p='emc.kb.mapping_log_db',t=tb) 
+            exec import_str
+            tablecls.__table__.drop(engine)
+#         Base.metadata.drop_all(engine)                                
 
     def empty_tables(self,tbls=None):
         """clear all db tables
@@ -24,6 +33,10 @@ class DbTools(object):
         items = []
         for tb in tbls:
             import_str = "from %(p)s import %(t)s as tablecls" % dict(p='emc.kb.mapping_db',t=tb) 
+            exec import_str
+            items.extend(Session.query(tablecls).all())
+        for tb in LOGTABLES:
+            import_str = "from %(p)s import %(t)s as tablecls" % dict(p='emc.kb.mapping_log_db',t=tb) 
             exec import_str
             items.extend(Session.query(tablecls).all())
         for m in items:
@@ -38,12 +51,59 @@ class DbTools(object):
         for tb in tbls:
             import_str = "from %(p)s import %(t)s as tablecls" % dict(p='emc.kb.mapping_db',t=tb) 
             exec import_str
-        Base.metadata.create_all(engine)
+            tablecls.__table__.create(engine)
+        for tb in LOGTABLES:
+            import_str = "from %(p)s import %(t)s as tablecls" % dict(p='emc.kb.mapping_log_db',t=tb) 
+            exec import_str
+            tablecls.__table__.create(engine)
+#         Base.metadata.create_all(engine)
 
-def main():
-    print("haha")
-    pass
-
+def main(action=None):  
+   
+    import sys
+    args = sys.argv
+    if len(args)  >1:
+        action = args[1]
+    else:
+        action = action or "create"
+    if action not in actions:
+        print("command line parameters is invalid!")
+        return 1
+    tool = DbTools()
+    if action =="create":
+        sure = raw_input("warning:This operation will create all database tables in oracle,are you sure?(yes/no):")
+        if sure.upper() == "YES" or  sure.upper()=="Y":
+            try:
+                tool.create_tables()
+                print("Oracle tables have been created successfully!")
+                return 0
+            except:
+                print("raise some errors")
+        print("None table has been created!")
+        return 1
+    elif action =="drop":
+        sure = raw_input("dangerous!!!:This operation will drop all database tables,are you sure?(yes/no):")
+        if sure.upper() == "YES" or  sure.upper()=="Y":
+            try:
+                tool.drop_tables()
+                print("All Oracle tables have been dropped successfully!")
+                return 0
+            except:
+                print("raise some errors")
+        print("None table has been dropped!")
+        return 1
+    else:
+        sure = raw_input("dangerous!!!:This operation will remove all database tables data,are you sure?(yes/no):")
+        if sure.upper() == "YES" or  sure.upper()=="Y":
+            try:
+                tool.empty_tables()
+                print("Oracle tables have been emptied successfully!")
+                return 0
+            except:
+                print("raise some errors")
+        print("None table has been emptied!")
+        return 1
+        
 if __name__ == '__main__':
     main()
         
